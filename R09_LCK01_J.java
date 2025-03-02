9703865b71083e4466a953e30f150e57233c352f
mattiasescudero <160158794+mattiasescudero@users.noreply.github.com>
2025-03-02 16:11:42 -0500

    Fixed Upload of R09_LCK_J.java

--- R09_LCK01_J.java
+++ R09_LCK01_J.java
@@ -1,12 +1,5 @@
-private final Boolean initialized = Boolean.FALSE;
- 
-public void doSomething() {
-  synchronized (initialized) {
-    // ...
-  }
-}
 private int count = 0;
-private final Integer Lock = count; // Boxed primitive Lock is shared
+private final Integer Lock = new Integer(count);
  
 public void doSomething() {
   synchronized (Lock) {
